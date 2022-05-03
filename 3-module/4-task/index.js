9bb94b88c5c7490c3991025b99a0af72e6a726e0
Regina Khozhaeva <khozhaevaregina@gmail.com>
2022-05-03 21:05:03 +0300

    решены 1-5 задачи

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,11 @@
 function showSalary(users, age) {
-  // ваш код...
+  let result = [];
+
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].age <= age) {
+      result.push(`${users[i].name}, ${users[i].balance}`);
+    }
+  }
+
+  return result.join('\n');
 }
