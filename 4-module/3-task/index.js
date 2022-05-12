e8e15c7921fe225c32fde7d1925cad9f60cefccd
Regina Khozhaeva <khozhaevaregina@gmail.com>
2022-05-12 20:33:42 +0300

    решены 1-3 задачи

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,23 @@
 function highlight(table) {
-  // ваш код...
+  let rows = table.rows;
+
+  for (let i = 1; i < rows.length; i++) {
+    if (rows[i].cells[3].dataset.available === 'true') {
+      rows[i].classList.add('available');
+    } else if (rows[i].cells[3].dataset.available === 'false') {
+      rows[i].classList.add('unavailable');
+    } else {
+      rows[i].hidden = true;
+    }
+
+    if (rows[i].cells[2].textContent === 'm') {
+      rows[i].classList.add('male');
+    } else if (rows[i].cells[2].textContent === 'f') {
+      rows[i].classList.add('female');
+    }
+
+    if (rows[i].cells[1].textContent < 18) {
+      rows[i].setAttribute('style', 'text-decoration: line-through');
+    } 
+  } 
 }
