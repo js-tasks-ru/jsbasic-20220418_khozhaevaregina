7c16d38d269d5368abd6efeb69c6e8e89520c548
Regina Khozhaeva <khozhaevaregina@gmail.com>
2022-05-27 12:20:08 +0300

    решены 1-3 задачи

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,24 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = document.createElement('table');
+    this.render(rows);
+    this.elem.addEventListener('click', (event) => this.onClick(event));
+  } 
+
+  render (array) {
+    let newRow = array
+      .map(row => `<tr><td>${row.name}</td><td>${row.age}</td><td>${row.salary}</td><td>${row.city}</td><td><button>X</button></td></tr>`)
+      .join('');
+    
+    this.elem.innerHTML = newRow;
+  }
+
+  onClick (event) {
+    let element = event.target.closest('TR');
+
+    if (event.target.tagName === 'BUTTON') {
+      element.remove();
+    }
   }
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -3,5 +3,87 @@ import createElement from '../../assets/lib/create-element.js';
 export default class Carousel {
   constructor(slides) {
     this.slides = slides;
+    this.currentIndex = 0;
+
+    this.carouse = createElement(
+      `<div class="carousel">
+     <div class="carousel__arrow carousel__arrow_right">
+       <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+     </div>
+     <div class="carousel__arrow carousel__arrow_left" style="display: none;" >
+       <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+     </div>
+   </div>`
+    );
+
+    this.carouselInner = createElement(`<div class="carousel__inner"></div>`);
+
+    for (const slide of this.slides) {
+      this.carouselInner.insertAdjacentHTML('beforeEnd',
+        `
+        <div class="carousel__slide" data-id="${slide.id}">
+            <img src="/assets/images/carousel/${slide.image}" class="carousel__img" alt="slide">
+            <div class="carousel__caption">
+              <span class="carousel__price">€${slide.price.toFixed(2)}</span>
+              <div class="carousel__title">${slide.name}</div>
+              <button type="button" class="carousel__button">
+                <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+              </button>
+            </div>
+          </div>
+        `
+      );
+    }
+
+    this.carouse.insertAdjacentElement('beforeEnd', this.carouselInner);
+
+    this.carouse.addEventListener('click', (event) => {
+      if (event.target.closest('.carousel__arrow_right')) {
+        this.currentIndex++;
+        this.setArrow();
+      }
+
+      if (event.target.closest('.carousel__arrow_left')) {
+        this.currentIndex--;
+        this.setArrow();
+      }
+    });
+
+    this.carouse.addEventListener('click', (event) => {
+      const carouselButton = event.target.closest('.carousel__button');
+
+      if (carouselButton) {
+        const productAddEventCarousel = new CustomEvent('product-add', {
+          detail: event.target.closest('.carousel__slide').dataset.id,
+          bubbles: true,
+        });
+        carouselButton.dispatchEvent(productAddEventCarousel);
+      }
+    });
+
+  }
+
+  get elem() {
+    return this.carouse;
+  }
+
+  setArrow() {
+    this.carouseArrowRight = document.querySelector('.carousel__arrow_right');
+    this.carouseArrowLeft = document.querySelector('.carousel__arrow_left');
+
+    this.offset = -this.carouselInner.offsetWidth * this.currentIndex;
+
+    this.carouselInner.style.transform = `translateX(${this.offset}px)`;
+    if (this.currentIndex === this.slides.length - 1) {
+      this.carouseArrowRight.style.display = 'none';
+    } else {
+      this.carouseArrowRight.style.display = '';
+    }
+
+    if (this.currentIndex === 0) {
+      this.carouseArrowLeft.style.display = 'none';
+    } else {
+      this.carouseArrowLeft.style.display = '';
+    }
   }
 }
